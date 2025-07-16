bb1f7b958a4f1926f6bb68afca581dc5f8fb1a67
Plasmat1x <damaghack@gmail.com>
2025-07-16 08:39:47 +0700

    up (Directx12Impl): Implement Fence, added FenceManager;

--- Parts/Directx12Impl/DX12Fence.cs
+++ Parts/Directx12Impl/DX12Fence.cs
@@ -6,42 +6,137 @@ using Silk.NET.Direct3D12;
 using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace Directx12Impl;
 public class DX12Fence: IFence
 {
-  private ComPtr<ID3D12Fence> p_fence;
-  private nint p_fenceEvent;
+  private readonly ComPtr<ID3D12Fence> p_fence;
+  private readonly AutoResetEvent p_fenceEvent;
   private ulong p_fenceValue;
+  private bool p_disposed;
     
-  public DX12Fence()
+  public DX12Fence(ComPtr<ID3D12Device>? _device, ulong _initialValue = 0)
   {
+    if(_device == null)
+      throw new ArgumentNullException(nameof(_device));
 
+    HResult hr = _device.Value.CreateFence(_initialValue,
+      FenceFlags.None,
+      out p_fence);
+
+    if(hr.IsFailure)
+      throw new COMException("Failed to create D3D12 fence", hr);
+
+    p_fenceValue = _initialValue;
+    p_fenceEvent = new AutoResetEvent(false);
   }
 
   public ulong Value  => p_fenceValue;
 
-  public bool IsSignaled => throw new NotImplementedException();
+  public bool IsSignaled
+  {
+    get
+    {
+      ThrowIfDisposed();
+      return p_fence.GetCompletedValue() >= p_fenceValue;
+    }
+  }
 
   public void Signal(ulong _value)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+
+    if(_value <= p_fenceValue)
+      throw new ArgumentException($"Signal value ({_value}) must be greater than current value ({p_fenceValue})");
+
+    p_fenceValue = _value;
+
+    HResult hr = p_fence.Signal(_value);
+    if(hr.IsFailure)
+      throw new COMException($"Failed to signal fence with value ({_value})", hr);
+
   }
 
-  public void Wait(ulong _value, uint _timeoutMs = uint.MaxValue)
+  public void SignalFromQueue(ComPtr<ID3D12CommandQueue>? _queue, ulong _value)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+
+    if(_queue == null)
+      throw new ArgumentNullException(nameof(_queue));
+
+    if(_value <= p_fenceValue)
+      throw new ArgumentException($"Signal value ({_value}) must be greater than current value ({p_fenceValue})");
+
+    p_fenceValue = _value;
+
+    HResult hr = _queue.Value.Signal(p_fence, _value);
+    if(hr.IsFailure)
+      throw new COMException($"Failed to signal fence from queue with value ({_value})", hr);
+
   }
 
-  public nint GetNativeHandle()
+  public unsafe void Wait(ulong _value, uint _timeoutMs = uint.MaxValue)
   {
-    throw new NotImplementedException();
+    ThrowIfDisposed();
+
+    if(p_fence.GetCompletedValue() >= _value)
+      return;
+
+    HResult hr = p_fence.SetEventOnCompletion(_value,
+      (void*)p_fenceEvent.SafeWaitHandle.DangerousGetHandle());
+
+    if(hr.IsFailure)
+      throw new COMException($"Failed to set event on fence completion for value {_value}", hr);
+
+    bool signaled = p_fenceEvent.WaitOne((int)_timeoutMs);
+
+    if(!signaled)
+      throw new TimeoutException($"Fence wait timed out after {_timeoutMs}ms waiting for value {_value}");
+  }
+
+  public ulong GetCompletedValue()
+  {
+    ThrowIfDisposed();
+    return p_fence.GetCompletedValue();
+  }
+
+  public IntPtr GetNativeHandle()
+  {
+    ThrowIfDisposed();
+    return Marshal.GetIUnknownForObject(p_fence);
+  }
+
+  public void WaitForQueue(ComPtr<ID3D12CommandQueue>? _queue, ulong _value)
+  {
+    ThrowIfDisposed();
+
+    if(_queue == null)
+      throw new ArgumentNullException(nameof(_queue));
+
+    HResult hr = _queue.Value.Wait(p_fence, _value);
+    if(hr.IsFailure)
+      throw new COMException($"Failed to make queue wait for fence value {_value}", hr);
   }
 
   public void Dispose()
   {
-    throw new NotImplementedException();
+    if(p_disposed)
+      return;
+
+    p_fenceEvent?.Dispose();
+    p_fence.Dispose();
+
+    p_disposed = true;
+    GC.SuppressFinalize(this);
+  }
+
+  private void ThrowIfDisposed()
+  {
+    if(p_disposed)
+      throw new ObjectDisposedException(nameof(DX12Fence));
   }
 }
---
+++ Parts/Directx12Impl/Managers/FrameFenceManager.cs
@@ -0,0 +1,59 @@
+using Silk.NET.Core.Native;
+using Silk.NET.Direct3D12;
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Directx12Impl.Managers;
+public class FrameFenceManager : IDisposable
+{
+  private readonly DX12Fence p_fence;
+  private readonly ulong[] p_fenceValues;
+  private readonly int p_frameCount;
+  private ulong p_currentFenceValue;
+  private int p_currentFrameIndex;
+
+  public FrameFenceManager(ComPtr<ID3D12Device> _device, int _frameCount = 3)
+  {
+    if(p_frameCount < 1)
+      throw new ArgumentException("Frame count must be at least 1", nameof(_frameCount));
+
+    p_frameCount = _frameCount;
+    p_fenceValues = new ulong[_frameCount];
+    p_fence = new DX12Fence(_device, 0);
+    p_currentFenceValue = 1;
+  }
+
+  public void WaitForPreviousFrame()
+  {
+    var fenceValueToWait = p_fenceValues[p_currentFrameIndex];
+    if(fenceValueToWait != 0)
+      p_fence.Wait(fenceValueToWait);
+  }
+
+  public void SignalEndOfFrame(ComPtr<ID3D12CommandQueue> _queue)
+  {
+    p_fence.SignalFromQueue(_queue, p_currentFenceValue);
+    p_currentFenceValue++;
+  }
+
+  public void MoveToNextFrame()
+  {
+    p_currentFrameIndex = (p_currentFrameIndex + 1) % p_frameCount;
+  }
+
+  public void WaitForGPU(ComPtr<ID3D12CommandQueue> _queue)
+  {
+    p_fence.SignalFromQueue(_queue, p_currentFenceValue);
+    p_fence.Wait(p_currentFenceValue);
+    p_currentFenceValue++;
+  }
+
+  public void Dispose()
+  {
+    p_fence?.Dispose();
+  }
+}
